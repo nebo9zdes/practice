bbadb70bc72b7e32ae56800686ab9eda8758f4c0
nebo9zdes <nebo.kotoroe.zdes@gmail.com>
2023-04-30 23:09:25 +0300

    first edition

---
+++ guessing_numbers_1.py
@@ -0,0 +1,16 @@
+import random
+
+num1 = random.randint(1, 100)
+num2 = int(input())
+
+while True:
+    if num1 == num2:
+        print('Вы угадали, поздравляем!')
+        break
+    
+    if num1 >= num2:
+        print('Слишком мало, попробуйте еще раз')
+
+    if num1 <= num2:
+        print('Слишком много, попробуйте еще раз')
+    num2 = int(input())
