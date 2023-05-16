50a463a23fa11512759e550ad4148e13ae1f7b4a
nebo9zdes <nebo.kotoroe.zdes@gmail.com>
2023-05-16 17:57:26 +0300

    added new game

---
+++ guessing_numbers_3.py
@@ -0,0 +1,43 @@
+import random
+print('Добро пожаловать в числовую угадайку')
+
+num1 = random.randint(1, 101)
+
+
+def is_valid(n):
+    return n.isdigit() and 1 <= int(n) <= 100
+
+def game():
+    while True:
+        num2 = input('Введите число от 1 до 100')
+        if is_valid(num2) == False:
+            print('А может быть все-таки введем целое число от 1 до 100?')
+            continue
+        else:
+            num2 = int(num2)
+            break
+
+    count = 0
+    while True:
+        count += 1
+        if num2 == num1:
+            print('Вы угадали, поздравляем!')
+            break
+        if num2 < num1:
+            print('Ваше число меньше загаданного, попробуйте еще разок')
+        if num2 > num1:
+            print('Ваше число больше загаданного, попробуйте еще разок')
+        num2 = int(input('Введите число от 1 до 100'))
+
+    print('Количество ваших попыток:', count)
+    print('Сыграем ещё?')
+
+game()
+
+while True:
+    answer = input('Введите Y, если согласны, или N, если нет')
+    if answer == 'Y':
+        game()
+    else:
+        print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+        break
